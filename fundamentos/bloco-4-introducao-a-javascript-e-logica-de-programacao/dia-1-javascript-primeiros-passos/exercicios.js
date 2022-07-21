38b2a9bd1dfef1c0e413d5ea28b8cb6f2e3e3383
Andrios Correa <andrioscorrea170685@gmail.com>
2022-07-21 11:08:02 -0300

    exercicios do bloco 4 dia 1.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
@@ -0,0 +1,62 @@
+// 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+// const a = 25;
+// const b = 20;
+
+// let soma = a + b;
+// console.log(soma);
+
+// let subtrai = a-b;
+// console.log(subtrai);
+
+// let multiplica = a * b;
+// console.log(multiplica);
+
+// let divide = a / b;
+// console.log(divide);
+
+// let modulo = 25 % 20;
+// console.log(modulo);
+
+// ___________________________________________________________________________________________
+
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+// const a = 10
+// const b = 20
+// if (a > 20){
+//     console.log(a)
+// }else{
+//     console.log(b)
+// }
+
+// 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+// const a = 5;
+// const b = 7;
+// const c = 10;
+
+// if (a > b && a > c){
+//     console.log(a)
+// }else if (b > a && b > c){
+//     console.log(b)
+// }else{
+//     console.log(c)
+// }
+
+// 4.Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+// const a = 10;
+
+// if (a > 0){
+//     console.log("positive")
+// }else if (a < 0){
+//     console.log("negative")
+// }else{
+//     console.log("zero")
+// }
