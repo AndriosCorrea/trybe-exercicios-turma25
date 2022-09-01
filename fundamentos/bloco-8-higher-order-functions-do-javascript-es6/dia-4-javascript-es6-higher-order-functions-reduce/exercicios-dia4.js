ccb9e1a27fdc035b3dc2fd8b72c1d0738da848e5
Andrios Correa <andrioscorrea170685@gmail.com>
2022-09-01 17:32:35 -0300

    exercicio 1 do bloco 8 dia 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios-dia4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios-dia4.js
@@ -1,1 +1,14 @@
 // Exercícios do bloco 8 dia 4
+
+// 1 - Dada uma matriz, transforme em um array.
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten(arrays) {
+     return arrays.reduce((acc, curr) => acc.concat(curr), [])
+ 
+  }
+  console.log(flatten(arrays))
